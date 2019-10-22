99a53b888f9d2df8e595591226fec218a9e3033e
mikelgomez <mikelgomez@opendeusto.es>
2019-10-22 08:46:06 +0200

    dfg

--- ejemplo/src/ejemplo/Ejemplo.java
+++ ejemplo/src/ejemplo/Ejemplo.java
@@ -11,13 +11,69 @@ public class Ejemplo extends JFrame{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	boolean stop = false;
+	int i = 0;
 	
 	public Ejemplo() {
 		
+		JPanel bPanel = new JPanel();
+		add(bPanel, BorderLayout.SOUTH);
 		
+		JButton button = new JButton("Click");
+		bPanel.add(button);
+		
+		JProgressBar progressBar = new JProgressBar(0, 10000000);
+		add(progressBar, BorderLayout.NORTH);
+		
+		progressBar.setValue(75);
+		
+		
+		
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Thread t = new Thread(new Runnable() {
+					
+					@Override
+					public void run() {
+						//boolean stop = false; // ?
+						//for (long i = 0; i< Long.MAX_VALUE && !stop; i++) {
+						
+						for (i = 0; i <= 10000000 && !stop; i++) {
+							System.out.println(i);
+							
+							SwingUtilities.invokeLater(new Runnable() {
+								
+								@Override
+								public void run() {
+									progressBar.setValue(i);
+									
+								}
+							});
+							
+
+						}
+						
+					}
+				});
+				
+				t.start();
+			}
+		});
+		
+		addWindowListener(new WindowAdapter() {
+			
+			public void windowClosing(WindowEvent e) {
+				
+				stop = true;
+			}
+			
+			
+		});
 		
 		this.setTitle("Ventana 2");
-		this.setSize(500, 500);
+		this.setSize(640, 480);
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	}
 	
@@ -25,7 +81,7 @@ public class Ejemplo extends JFrame{
 		Ejemplo ejemplo = new Ejemplo();
 		ejemplo.setVisible(true);
 
-		System.out.println("Hola mundo desde Java");
+		//System.out.println("Hola mundo desde Java"); fdgdfg
 		
 	}
 	
